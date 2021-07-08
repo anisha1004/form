d0749ec3090c7112f4e5e55375f12c1cee22e846
Arsh-ak7 <arshkr07@gmail.com>
2021-07-08 17:12:04 +0530

    Upload fixes

--- src/Card.js
+++ src/Card.js
@@ -1,44 +1,95 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import { Button } from '@material-ui/core';
-import UploadButtons from './UploadButton';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import { Button } from "@material-ui/core";
+import storage from "./firebase";
+//import UploadButtons from "./UploadButton";
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 1000,
-  },
-  media: {
-    width: 250,
-    height: 250,
-  },
-});
+const useStyles = makeStyles((theme) => ({
+	root: {
+		maxWidth: 1000,
+	},
+	media: {
+		width: 250,
+		height: 250,
+	},
+	root_b: {
+		"& > *": {
+			margin: theme.spacing(1),
+		},
+	},
+	input: {
+		display: "none",
+	},
+}));
 
 export default function MediaCard() {
-  const classes = useStyles();
+	const classes = useStyles();
+	const [image, setImage] = useState("");
+	const [url, setUrl] = useState("");
+	const upload = () => {
+		if (image === null) return;
+		storage.ref(`/images/${image.name}`).put(image);
+	};
+	return (
+		<Card className={classes.root}>
+			<CardActionArea>
+				<CardMedia
+					className={classes.media}
+					image={url}
+					title='Contemplative Reptile'
+				/>
+				{/* <img src={url} className='image-url' /> */}
+				<CardContent></CardContent>
+			</CardActionArea>
 
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          
-        </CardContent>
-      </CardActionArea>
-
-      <CardActions>
-        <UploadButtons />
-        <Button>Remove</Button> 
-      </CardActions>
-
-    </Card>
-  );
+			<CardActions>
+				{/* <UploadButtons /> */}
+				{/* <input
+					onChange={(e) => {
+						setImage(e.target.files[0]);
+						setUrl(URL.createObjectURL(e.target.files[0]));
+					}}
+					className={classes.input}
+					id='contained-button-file'
+					multiple
+					type='file'
+				/>
+				<Button
+					variant='contained'
+					color='primary'
+					component='span'
+					onClick={upload}>
+					Upload
+				</Button> */}
+				<div className={classes.root_b}>
+					<input
+						accept='image/*'
+						onChange={(e) => {
+							setImage(e.target.files[0]);
+							setUrl(URL.createObjectURL(e.target.files[0]));
+						}}
+						className={classes.input}
+						id='contained-button-file'
+						multiple
+						type='file'
+					/>
+					<label htmlFor='contained-button-file'>
+						<Button
+							variant='contained'
+							color='primary'
+							onClick={upload}
+							component='span'>
+							Upload
+						</Button>
+					</label>
+				</div>
+				<Button>Remove</Button>
+			</CardActions>
+		</Card>
+	);
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCArxHV69_f-Gb-nulxaBJaacTfOeW6tcs",
-    authDomain: "campaign-form.firebaseapp.com",
-    projectId: "campaign-form",
-    storageBucket: "campaign-form.appspot.com",
-    messagingSenderId: "558427473664",
-    appId: "1:558427473664:web:3e6085fa296d376f3a15e5",
-    measurementId: "G-Z6Z2YHZW49"
-  };
+	apiKey: "AIzaSyCyNbGxk5JLVIP4PIva_i66Gpv9li3bRqE",
+	authDomain: "airbnb-clone-f74de.firebaseapp.com",
+	databaseURL: "https://airbnb-clone-f74de.firebaseio.com",
+	projectId: "airbnb-clone-f74de",
+	storageBucket: "airbnb-clone-f74de.appspot.com",
+	messagingSenderId: "139838569942",
+	appId: "1:139838569942:web:9b859a6635414f12a57877",
+	measurementId: "G-JJDZL6PEHE",
+};
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
-const db = firebaseApp.firestore();
-export default db;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const storage = firebaseApp.storage();
+export default storage;
